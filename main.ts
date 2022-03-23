d301b63c0651c377c2e33a7748a386637ede31a4
MilanKaa45 <102212343+MilanKaa45@users.noreply.github.com>
2022-03-23 08:25:01 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    cuteBot.moveTime(cuteBot.Direction.forward, 50, 2)
+    cuteBot.turnright()
+    cuteBot.moveTime(cuteBot.Direction.forward, 50, 2)
+    cuteBot.moveTime(cuteBot.Direction.forward, 50, 2)
+    cuteBot.turnleft()
+    cuteBot.moveTime(cuteBot.Direction.forward, 50, 2)
+})
